655d33412ee680d70a7dcaa5df1b054e6697f87c
P V Nikhilanj <nikhilanj97@gmail.com>
2016-09-15 14:55:36 +0530

    Improved readability, made few changes.
    Changed variable names to proper ones,
    Added comments where required.
    Added checks for 'already present' and 'empty' files.

--- cms_login.py
+++ cms_login.py
@@ -1,27 +1,23 @@
+###CMS downloader###
+##The script logs in to CMS, and checks the 'Activity' area for new files.
+##If there are any files, it will download them to user specified path.Most forum posts are written to a txt file in the same path.
+
 import mechanize
-from bs4 import BeautifulSoup
 import cookielib
+from bs4 import BeautifulSoup
 import os
-import requests
 from sys import exit
+from site_test import site_check
 
-url1='http://cms.bits-hyderabad.ac.in/moodle/login/index.php'
-url2='http://172.16.100.125/'
+cms_general='http://cms.bits-hyderabad.ac.in/moodle/login/index.php'
+cms_intranet='http://172.16.100.125/'
 
-def is_site_working(url):
-    try:
-        response=requests.get(url,timeout=10)
-        if(int(response.status_code)==200):
-            return True
-    except ((requests.ConnectionError) or (requests.HTTPError)):
-        return False
-
-if(is_site_working(url1)):
-    finalsite=url1
-    print "Connected to CMS.Let's start."
-elif(is_site_working(url2)):
-    print "Connected to CMS.Let's start."
-    finalsite=url2
+if(site_check(cms_general)):
+    print 'Connected to CMS'
+    workingsite=cms_general
+elif(site_check(cms_intranet)):
+    print 'Connected to CMS via intranet'
+    workingsite=cms_intranet      
 else:
     print "Unable to connect to CMS.Try again later."
     print "Exiting script for now."
@@ -32,107 +28,132 @@ br.set_handle_robots(False)
 cj = cookielib.CookieJar()
 br.set_cookiejar(cj)
 
-if(finalsite==url2): 
-    site=br.open(finalsite).read()
-    soup = BeautifulSoup(site, 'html.parser')
-    req = br.click_link(text='Proceed')
-    br.open(req)
-    #print br.geturl()
-    sitef=br.click_link(text='Log in')
-    br.open(sitef)
-    finalsite=br.geturl()
+##Intranet site leads to an intial landing page,having a 'Proceed' button.
 
-print "Opening %s "%(finalsite)
-url=br.open(finalsite)
+if(workingsite==cms_intranet): 
+    site=br.open(workingsite).read()
+    proceed = br.click_link(text='Proceed')
+    br.open(proceed)
+    site2=br.click_link(text='Log in')
+    br.open(site2)
+    workingsite=br.geturl()
 
-if(os.path.isfile('CMS_userdata.dat')):
-    f=open('CMS_userdata.dat','r')
-    data=f.read()
-    user,pwd=map(str,data.split(' '))
-    f.close()
-    
+print "Opening %s "%(workingsite)
+br.open(workingsite)
+
+##Fill login form.Login details stored in CMS_userdata.dat
+if(os.path.isfile('CMS_userdata.dat') and os.stat('CMS_userdata.dat').st_size!=0):
+    datafile=open('CMS_userdata.dat','r')
+    userdata=datafile.read()
+    user,pwd=map(str,userdata.split(','))
+    datafile.close()
 else:
     cms_username=raw_input("Enter your CMS Username :     ")
     cms_password=raw_input("Enter your CMS Password :     ")
     file_write=raw_input("Do you want me to remember these details ?(Y/N)   ")
     
     if(file_write.lower()=='y'):
-        with open('CMS_userdata.dat', 'w') as f:
-            f.write(str(cms_username)+' '+str(cms_password))
+        with open('CMS_userdata.dat', 'wb') as datafile:
+            datafile.write(str(cms_username)+','+str(cms_password))
     user,pwd=cms_username,cms_password
-
+    ##Include option to overwrite,login as different user.
+    ##May have to change data storage type to recognise different users.
 
 br.select_form(nr=0)
-print "Logging in as %s . . . . . "%str(user)
+print "Logging in as %s . . . . . \n"%str(user)
 br.form['username'] = user
 br.form['password'] = pwd
 response=br.submit()
 site=response.read()
-soup = BeautifulSoup(site, 'html.parser')
+##
 
-usename=soup.body.find('div', attrs={'id':'page-content'})
+UserLandingPageSoup = BeautifulSoup(site, 'html.parser')
+usename=UserLandingPageSoup.body.find('div', attrs={'id':'page-content'})
 print "Hello %s !"%(' '.join((usename.h3.string).split(' ')[:-1]))
 
-courselist= soup.body.find('div', attrs={'class':'course_list'})
-under_courses=courselist.find_all('div', attrs={'class':'box coursebox'})
+courselist= UserLandingPageSoup.body.find('div', attrs={'class':'course_list'})
+##courselist gets all the courses the user has enrolled in.
+course_boxes=courselist.find_all('div', attrs={'class':'box coursebox'})
+##course_links gets the 'box coursebox' area,where links and name are shown.
 
-for i in under_courses:
-    morelol=i.find_all('a')
+##Todo : Store the list of all enrolled courses along with userdata.
+##Check if user has not enrolled in any new course.If not, then jump directly to checking,instead of getting each box everytime.
 
-    for j in morelol:
-        course_name=j.get('title')
-        course_site=j.get('href')
+for coursebox in course_boxes:
+    course_links=coursebox.find_all('a')
+    ##Link to the course page is found.
 
-        url=br.open(course_site).read()
-        soup1 = BeautifulSoup(url, 'html.parser')
-        activity=soup1.body.find('div',attrs={'class':'block_recent_activity  block'})
-        time=activity.find('div',attrs={'class':'activityhead'})
+    for course in course_links:
+        course_name=course.get('title')
+        course_site=course.get('href')
+        print "Checking %s ......"%(course_name)
+        ##Check the 'recent activity' block for any new files.
+        ##The website gives details for the past 48 hrs,or since the last login,whichever is earlier.
+        course_site_read=br.open(course_site).read()
+        course_soup = BeautifulSoup(course_site_read, 'html.parser')
+        course_activity=course_soup.body.find('div',attrs={'class':'block_recent_activity  block'})
+        time=course_activity.find('div',attrs={'class':'activityhead'})
         timestamp=time.string
-
-        if(activity.p.string=='No recent activity'):
-            print 'No recent %s in %s'%(timestamp[:1].lower()+timestamp[1:],course_name)
-
+        
+        if(course_activity.p.string=='No recent activity'):
+            print 'No recent %s here :) \n'%(timestamp[:1].lower()+timestamp[1:])
+            ##timestamp is stored as 'Activity since '
         else:
-            files=activity.find_all("p", "activity")
-
-            for fil in files:
-                fil_name=fil.a.string
-                dwfile=fil.a.get('href')
-                print fil_name
-                #print dwfile
-
-                dw=br.open(dwfile)
-                ref=br.geturl()
-                br.open(ref)
+            new_files=course_activity.find_all("p", "activity")
+            for new_file in new_files:
+                try:
+                    file_name=new_file.a.string
+                except AttributeError:
+                    continue
+                dwldfile=new_file.a.get('href')
+                print file_name
+                br.open(dwldfile)
+                ##In most cases,this directs to a document file.Tested on .pdf,.docx ,the most common formats.
+                document=br.geturl()
+                br.open(document)
 
                 dwl_path='C:\\Users\\Nikhilanj\\Desktop\\'
-                save_filename=dwl_path+fil_name+'.'+ref.split('.')[-1]
+                save_filename=file_name+'.'+ref.split('.')[-1]
 
                 try:
-                    if(os.path.isfile(save_filename)):
-                        print 'File %s already exists'%(save_filename)
+                    if(os.path.isfile(dwl_path+save_filename) and os.stat(dwl_path+save_filename).st_size!=0):
+                        print 'File %s already exists at %s'%(save_filename,dwl_path)
                     else:
-                        br.retrieve(ref,save_filename)
-                        print '%s saved'%(save_filename)       
+                        br.retrieve(ref,dwl_path+save_filename)
+                        ##This downloads the file to dwl_path.
+                        print '%s saved to '%(save_filename,dwl_path)       
 
                 except IOError:
 
-                    if('forum' in ref):
-                        forum_post=br.open(ref).read()
-                        postsoup=BeautifulSoup(forum_post,'html.parser')
-                        post=postsoup.body.find('div',attrs={'role':'main'})
-                        thing_to_write=post.text.split('(')[0]
-                        ##this is to take care of
-                        ##'(There are no discussion topics yet in this forum)' text
-                        save_post_with_name=dwl_path+fil_name+'.txt'
+                    if('forum' in document):
+                        try:
+                            forum_post=br.open(ref).read()
+                            postsoup=BeautifulSoup(forum_post,'html.parser')
+                            post=postsoup.body.find('div',attrs={'role':'main'})
+                            text_to_write=' '.join(post.text.split('(')[:-1])
+                            ##this is to take care of
+                            ##'(There are no discussion topics yet in this forum)' text.
+                            ##This may give some issues in some cases.Have to find another way maybe.
+                            post_read_error_flag=1
+                            if(text_to_write==''):
+                                print 'There was an error reading the forum post. Please check manually at '+ref+' .Sorry :( '
+                                post_read_error_flag=0
+                            
 
-                        if(os.path.isfile(save_post_with_name)):
-                            print 'File %s already exists'%(save_post_with_name)
-                        else:
-                            with open(save_post_with_name, 'w') as g:
-                                g.write(thing_to_write)
-                                print 'Forum post written to %s'%(save_post_with_name)
+                            save_post_with_name=fil_name+'.txt'
 
-                    if('folder' in ref):
+                            if(os.path.isfile(dwl_path+save_post_with_name) and os.stat(dwl_path+save_filename).st_size!=0 and post_error_read_flag):
+                                print 'File %s already exists at %s'%(save_post_with_name,dwl_path)
+                            else:
+                                if(post_read_error_flag):
+                                    with open(dwl_path+save_post_with_name, 'w') as g:
+                                        g.write(thing_to_write)
+                                        print 'Forum post written to %s at %s'%(save_post_with_name,dwl_path)
+                        except :
+                            print 'There was an error reading the post'
+
+                    if('folder' in document):
                         print 'Unable to download folder.'##Adjust for now.
-                    
+                        ##Downloading folders is not supported yet.
+                        ##The 'download folder' button is a submit only form(SubmitControl).
+                        ##Unable to submit it for some reason.
